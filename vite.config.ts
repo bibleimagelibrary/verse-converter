117d96a901045eea5d39de7df5c76875cb7f519c
MaggieCampo <maggiecampo25@gmail.com>
2026-04-02 12:36:13 -0400

    Fixes for postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/verse-converter/" : "/",
-  plugins: [tailwindcss()],
   server: {
     port: 5173,
     open: true,
